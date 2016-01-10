0d1bd04d962f0e6ebf4e435edf7e481237cbf843
pi2ner <9pioner9@gmail.com>
2016-01-10 19:47:28 +0200

    fixed HTML tags, CSS. *.js file added.

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by anna on 12/27/15.
+ */
+/**window.onload=function() {
+    var button = document.getElementById("interactive-button");
+    button.addEventListener("click", function () {
+        alert("Wow")
+    }, false);
+}**/
+
+
+$(document).ready( function() {
+        $('.interactive-button').on('click', function() {
+            alert("Wow");
+        });
+
+    });
+
